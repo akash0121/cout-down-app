09e4585d66fe415a318ec60d5e004463ff6c8fda
akash0121 <97994696+akash0121@users.noreply.github.com>
2023-04-21 04:04:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+let endDate = prompt("Please enter the ( date Month-name year time am/pm )");
+//  const endDate = "25 April 2023 03:00 am";
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+// const clock = () =>{
+
+// }
+
+function clock(){
+    const end = new Date(endDate);
+    const now =  new Date();
+    const diff = (end-now)/1000;
+    if(diff<0) return;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor( diff/3600)%24;
+    inputs[2].value = Math.floor(diff/60) %60;
+    inputs[3].value = Math.floor(diff) %60;
+}
+
+clock();
+
+setInterval(
+    () =>{
+        clock();
+    }, 
+    1000
+)
+
